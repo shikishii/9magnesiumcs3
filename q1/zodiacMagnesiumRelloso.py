ba4eb536ffec3e31f90a11398711380ab1283323
shikishii <kgsrelloso@brc.pshs.edu.ph>
2026-08-24 14:54:38 +0000

    Add zodiac sign calculation based on birth year

--- q1/zodiacMagnesiumRelloso.py
+++ q1/zodiacMagnesiumRelloso.py
@@ -1,1 +1,11 @@
+year = int(input("Enter year of birth (not earlier than 1900): "))
 
+if year < 1900:
+    print("Invalid Year, input should not be earlier than 1900.")
+
+else:
+    zodiac = ["Rat (鼠 / Shǔ)", "Ox (牛 / Niú)", "Tiger (虎 / Hǔ)", "Rabbit (兔 / Tù)", "Dragon (龙 / Lóng)", "Snake (蛇 / Shé)", "Horse (马 / Mǎ)", "Goat (羊 / Yáng)", "Monkey (猴 / Hóu)", "Rooster (鸡 / Jī)", "Dog (狗 / Gǒu)", "Pig (猪 / Zhū)"]
+    zodiac_index = (year - 1900)%12
+    element_index = ((year - 1900)//2)%5
+
+    print(f"Your Chinese Zodiac sign is: {zodiac[zodiac_index]}")
